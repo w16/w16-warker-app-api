1c5a8914862fcba12684b4757aaa24aa8f3955bb
Yago Rezende <ydrdeveloper@outlook.com>
2020-12-20 10:21:51 -0300

    Migrations, models e controllers criados

---
+++ database/migrations/2020_12_20_125646_create_postos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cidade_id')->constrained();
+            $table->unsignedInteger('reservatorio');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postos');
+    }
+}
