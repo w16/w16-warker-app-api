aa16f2f0fa92aed6c30a8542a2ed1ddf5134f46e
Rodrigo Medeiros <rodrigomedeiros.dev@gmail.com>
2021-03-10 01:04:37 -0300

    Controller e Rotas API de Cidades

---
+++ app/Http/Controllers/CidadeController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CidadeService;
+use Illuminate\Http\Request;
+use Exception;
+
+class CidadeController extends Controller
+{
+    /**
+     * @var cidadeService
+     */
+    protected $cidadeService;
+
+    /**
+     * CidadeController Construtor
+     *
+     * @param CidadeService $cidadeService
+     *
+     */
+    public function __construct(CidadeService $cidadeService)
+    {
+        $this->cidadeService = $cidadeService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->cidadeService->getAll();
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->only([
+            'nome_da_cidade',
+            'latitude',
+            'longitude'
+        ]);
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->cidadeService->saveCidade($data);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->cidadeService->getById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->only([
+            'nome_da_cidade',
+            'latitude',
+            'longitude'
+        ]);
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->cidadeService->updateCidade($data, $id);
+
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->cidadeService->deleteById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+}
