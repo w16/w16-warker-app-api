3488c5dbe33af39293a7b8ac358a744cf7c41a25
Rodrigo Medeiros <rodrigomedeiros.dev@gmail.com>
2021-03-10 00:57:02 -0300

    Repository e Resource de Cidades

---
+++ app/Http/Resources/CidadeCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CidadeCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'cidade' => $this->nome_da_cidade,
+            'coords' => [
+                'latitude' => $this->latitude,
+                'longitude' => $this->longitude,
+            ],
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
