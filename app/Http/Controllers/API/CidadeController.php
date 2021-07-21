1367cf5ae7e9e731dc1502000789e48e8625c2ee
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-07-21 11:33:46 -0300

    Create controllers

---
+++ app/Http/Controllers/API/CidadeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCidadeRequest;
+use App\Http\Requests\UpdateCidadeRequest;
+use App\Services\CidadeService;
+use Illuminate\Http\Request;
+
+class CidadeController extends Controller
+{
+    protected $service;
+
+    public function __construct(CidadeService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \App\Http\Resources\Object
+     */
+    public function index()
+    {
+        return $this->service->getAll();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\CreateCidadeRequest  $request
+     * @return \App\Http\Resources\Object
+     */
+    public function store(CreateCidadeRequest $request)
+    {
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function show($id)
+    {
+        return $this->service->get($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCidadeRequest  $request
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function update(UpdateCidadeRequest $request, $id)
+    {
+        return $this->service->update($id, $request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function destroy($id)
+    {
+        return $this->destroy->get($id);
+    }
+}
---
+++ app/Http/Controllers/API/PostoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePostoRequest;
+use App\Http\Requests\UpdatePostoRequest;
+use Illuminate\Http\Request;
+
+class PostoController extends Controller
+{
+    protected $service;
+
+    public function __construct(PostoService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \App\Http\Resources\Object
+     */
+    public function index()
+    {
+        return $this->service->getAll();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\CreatePostoRequest $request
+     * @return \App\Http\Resources\Object
+     */
+    public function store(CreatePostoRequest $request)
+    {
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function show($id)
+    {
+        return $this->service->get($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostoRequest $request
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function update(UpdatePostoRequest $request, $id)
+    {
+        return $this->service->update($id, $request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \App\Http\Resources\Object
+     */
+    public function destroy($id)
+    {
+        return $this->destroy->get($id);
+    }
+}
