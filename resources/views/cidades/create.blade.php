d4ccbb733e8f517c39619fc9f44d5f020d20c18e
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-07-22 11:39:45 -0300

    Rename title

--- resources/views/cidades/create.blade.php
+++ resources/views/cidades/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-center text-gray-800 leading-tight">
-            Criando registro de cidade
+            Novo registro de cidade
         </h2>
     </x-slot>
     <div>
