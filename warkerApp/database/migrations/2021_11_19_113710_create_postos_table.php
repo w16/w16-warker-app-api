458d70551f1a2969cdee63e8288c60f3fef48713
Alan Tavares <alan_tj@hotmail.com.br>
2021-11-19 09:55:36 -0300

    models, controller, seeder, migrations

---
+++ warkerApp/database/migrations/2021_11_19_113710_create_postos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cidade_id');
+            $table->foreign('cidade_id')->references('id')->on('cidades');
+            $table->integer("reservatorio");
+            $table->double("latitude");
+            $table->double("logintude");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postos');
+    }
+}
--- warkerApp/database/seeders/CidadeSeeder.php
+++ warkerApp/database/seeders/CidadeSeeder.php
@@ -1,30 +1,27 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 
-class DatabaseSeeder extends Seeder
+class CidadeSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        for($i=0; $i<100; $i++){
+            for($i=0; $i<100; $i++){
             DB::table('cidades')->insert([
                 'nome' => Str::random(10),
                 'latitude' => rand(0,100),
-                'logintude' => rand(0,100),
-                
+                'logintude' => rand(0,100)
 
             ]);
-        }      
+        }
     }
 }
