549b045ff6a64c5635a677863441807af6817146
Cleidson Souza <cleidson.santos.souza@gmail.com>
2022-05-12 15:16:34 -0300

    Criação do bando de dados com verificação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 engine = create_engine('sqlite:///combustivel.db')
 
@@ -10,7 +11,9 @@ session = Session()
 Base = declarative_base()
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    arquivo = 'combustivel.db'
+    if not os.path.isfile(arquivo):
+        Base.metadata.create_all(bind=engine)
 
 if __name__ == '__main__':
     init_db()
