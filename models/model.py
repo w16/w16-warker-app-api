636cc01e424137ab3308d45e521fbb0934cd721b
kirestein <erik.proenca2011@gmail.com>
2022-05-14 16:59:48 -0300

    Updating models postos

--- models/model.py
+++ models/model.py
@@ -16,16 +16,16 @@ class Cidades(ormar.Model):
   created_at: Timestamp
   updated_at: Timestamp
 
-  class Postos(ormar.Model):
-    class Meta:
-      metadata = metadata
-      database = database
-      tablename = 'cidades'
+class Postos(ormar.Model):
+  class Meta:
+    metadata = metadata
+    database = database
+    tablename = 'cidades'
 
-    id: int = ormar.Integer(primary_key=True)
-    reservatorio: int = ormar.Integer() # vai ser representado em porcentagem
-    latitude: float = ormar.Float()
-    longitude: float = ormar.Float()
-    created_at: Timestamp
-    updated_at: Timestamp
-    cidade_id: int #FOREIGN KEY
+  id: int = ormar.Integer(primary_key=True)
+  reservatorio: int = ormar.Integer() # vai ser representado em porcentagem
+  latitude: float = ormar.Float()
+  longitude: float = ormar.Float()
+  created_at: Timestamp
+  updated_at: Timestamp
+  cidade_id: int = ormar.ForeignKey(Cidades) #FOREIGN KEY
