bd2b3b0d86ea475dfb87a03a3555fe3d9a3d66a1
kirestein <erik.proenca2011@gmail.com>
2022-05-14 17:42:12 -0300

    Created and edited rotas file

---
+++ rotas.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from controllers import cidades_controllers as cidades, postos_controllers as postos
+
+router = APIRouter()
+
+# * definição das rotas
+router.include_router(cidades.router, prefix='/cidades') #rota para cidades
+router.include_router(postos.router, prefix='/postos') #rota para postos
