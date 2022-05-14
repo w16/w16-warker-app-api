78f88c41516037cb224ddd9ef7841af90e01bf39
kirestein <erik.proenca2011@gmail.com>
2022-05-14 17:43:39 -0300

    updating main file to receive routes

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
-from sqlite3 import Timestamp
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-import ormar
+from rotas import router
+
 
 app = FastAPI()
 
-# ! Rota raiz
-@app.get('/')
-def raix():
-  return {'Olá': 'Mundo'}
-
-# * criar model
-class Cidades(ormar.Model):
-  id: int = ormar.Integer(primary_key=True)
-  nome_da_cidade: str = ormar.String(max_length=150)
-  latitude: float = ormar.Float()
-  longitude: float = ormar.Float()
-  created_at: Timestamp
-  updated_at: Timestamp
-
-class Postos(ormar.Model):
-  id: int = ormar.Integer(primary_key=True)
-  cidade_id: int = ormar.Integer(foreign_key=True) #FOREIGN KEY
-  reservatorio: int = ormar.Integer() # vai ser representado em porcentagem
-  latitude: float = ormar.Float()
-  longitude: float = ormar.Float()
-  created_at: Timestamp
-  updated_at: Timestamp
-
-banco_de_dados = []
-
-@app.post('/cidades')
-def add_cidade(cidade: Cidades):
-  banco_de_dados.append(cidade)
-  return cidade
-
-@app.get('/cidades')
-def list_cidades():
-  return banco_de_dados
+app.include_router(router, prefix='')
