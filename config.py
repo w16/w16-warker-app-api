7c1f3f91ca1a8f6d26283d2e8334f3a9ea195db1
kirestein <erik.proenca2011@gmail.com>
2022-05-14 16:17:28 -0300

    MAKIGN A COMMENT

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import databases
 import sqlalchemy
 import os
 
+#? Aqui neste arquivo ficará a nossa configuração para conecção e teste do Banco de Dados 
 DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
 TEST_DATABASE = os.getenv('TEST_DATABASE', 'false') in ('true', 'yes')
 database = databases.Database(DATABASE_URL, force_rollback=TEST_DATABASE)
